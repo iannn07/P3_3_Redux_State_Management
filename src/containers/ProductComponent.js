cc5427e2737a4709193cd29453e4f852576c1c03
Iannn <pristianb007@gmail.com>
2024-03-08 16:23:14 +0700

    Fixed CSS

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,21 +1,32 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
-  const { id, title } = products[0];
-  return (
-    <div className="four column wide">
-      <div className="ui link cards">
-        <div className="card">
-          <div className="image"></div>
-          <div className="content">
-            <div className="header">{title}</div>
+
+  const renderList = products.map((product) => {
+    return (
+      <div className="four wide column" key={product.id}>
+        <Link to={`/product/${product.id}`}>
+          <div className="ui link cards">
+            <div className="card">
+              <div className="image">
+                <img src={product.image} alt="" />
+              </div>
+              <div className="content">
+                <div className="header">{product.title}</div>
+                <div className="meta price">$ {product.price}</div>
+                <div className="meta">{product.category}</div>
+              </div>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
-    </div>
-  );
+    );
+  });
+
+  return renderList;
 };
 
 export default ProductComponent;
--- src/containers/ProductList.js
+++ src/containers/ProductList.js
@@ -1,15 +1,18 @@
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import ProductComponent from "./ProductComponent";
+import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
+import ProductComponent from "./ProductComponent";
+import { setProducts } from "../redux/actions/productActions";
 
 const ProductList = () => {
   const products = useSelector((state) => state);
+  const dispatch = useDispatch();
 
   const fetchProducts = async () => {
     try {
       const response = await axios.get("https://fakestoreapi.com/products");
       console.log(response.data);
+      dispatch(setProducts(response.data));
     } catch (error) {
       console.log("Error", error.message);
       throw error;
@@ -19,7 +22,7 @@ const ProductList = () => {
     fetchProducts();
   }, []);
 
-  console.log(products);
+  console.log("Products: ", products);
 
   return (
     <div className="ui grid container">
